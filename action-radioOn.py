9ec5983415571bdfe200d8df479c4adb90e64c10
Leon Weber <leonweber@posteo.de>
2018-06-07 00:02:29 +0200

    Fix stuff

--- action-radioOn.py
+++ action-radioOn.py
@@ -36,13 +36,15 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     current_session_id = intentMessage.session_id
-    print "received intent:"
-    print intentMessage
-    hermes.publish_end_session(current_session_id, "radioOn")
+    radio_name = intentMessage.slots.radio_name[0].raw_value
+    if radio_name in RADIOS:
+        subprocess.call(["mplayer", RADIOS[radio_name]])
+    else:
+        hermes.publish_end_session(current_session_id, "Sorry, I could not find a radio with the name " + radio_name)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("CrystalMethod:hello", subscribe_intent_callback) \
+        h.subscribe_intent("nodewig:radioOn", subscribe_intent_callback) \
          .start()
 
